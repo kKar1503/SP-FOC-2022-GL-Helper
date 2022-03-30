6d8e79cd10a5855b8f8378fc79090a7caaacb4e6
kKar1503 <isonice4572@gmail.com>
2022-03-31 03:23:03 +0800

    Added reaction command placeholder

---
+++ src/commands/reaction.js
@@ -0,0 +1,15 @@
+// Require discordjs/builders for building slash commands
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+// Require modules for generating embeds
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder().setName('ping').setDescription('Pings the bot.'),
+	async execute(interaction) {
+		await interaction.reply({
+			content: 'Pong!',
+			ephemeral: true,
+		});
+	},
+};
