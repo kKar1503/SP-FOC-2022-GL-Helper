1ba45fec23e0392aff9bc060f54d1bfcea294de7
kKar1503 <isonice4572@gmail.com>
2022-03-31 03:35:46 +0800

    Added reaction command
    Reaction command to add reaction to the games polling for GL games.

--- src/commands/reaction.js
+++ src/commands/reaction.js
@@ -2,13 +2,19 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 // Require modules for generating embeds
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, Message } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder().setName('ping').setDescription('Pings the bot.'),
+	data: new SlashCommandBuilder()
+		.setName('react')
+		.setDescription('Reaction for GL Games.')
+		.addIntegerOption((option) => option.setName('msgId').setDescription('ID of the message from bot.').setRequired(true)),
 	async execute(interaction) {
+		let msgId = interaction.options.getInteger('msgId');
+		let message = interaction.channel.messages.fetch(msgId);
+		console.log(message);
 		await interaction.reply({
-			content: 'Pong!',
+			content: 'Done!',
 			ephemeral: true,
 		});
 	},
