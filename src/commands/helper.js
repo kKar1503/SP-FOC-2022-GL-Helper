dc05b654f5434b828038a1b269d0d3bb984fa166
kKar1503 <isonice4572@gmail.com>
2022-03-31 01:33:21 +0800

    Modified test Helper Statement
    Added input in reply.

--- src/commands/helper.js
+++ src/commands/helper.js
@@ -8,8 +8,9 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
 		.setDescription('Sends helper statement.')
-		.addIntegerOption((option) => option.setName('Statement').setDescription('Statement number for the helper lines').setRequired(true)),
+		.addIntegerOption((option) => option.setName('statement').setDescription('Statement number for the helper lines').setRequired(true)),
 	async execute(interaction) {
-		await interaction.reply('Helper');
+		let statementNumber = interaction.options.getInteger('statement');
+		await interaction.reply(`Helper Statement ${statementNumber}`);
 	},
 };
