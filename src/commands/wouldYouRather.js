162683c2f4165c9ce3bf4d1a69fd8d4b4828c75e
kKar1503 <isonice4572@gmail.com>
2022-03-31 04:20:57 +0800

    Added would you rather command
    Would you rather command takes in 1 argument of question number and reply with question statement and add reactions for users' input.
    Add npm package ordinal for displaying ordinal, i.e. 1st, 2st, 3rd...

--- src/commands/wouldYouRather.js
+++ src/commands/wouldYouRather.js
@@ -1,12 +1,27 @@
 // Require discordjs/builders for building slash commands
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
+// Import would you rather questions
+const { wouldYouRather } = require('./wouldYouRather.json');
+
+// Ordinal module
+const ordinal = require('ordinal');
+
 module.exports = {
-	data: new SlashCommandBuilder().setName('ping').setDescription('Pings the bot.'),
+	data: new SlashCommandBuilder()
+		.setName('wyr')
+		.setDescription('Would you rather?')
+		.addIntegerOption((option) => option.setName('question').setDescription('Question number for Would You Rather').setRequired(true)),
 	async execute(interaction) {
-		await interaction.reply({
-			content: 'Pong!',
-			ephemeral: true,
-		});
+		let questionNumber = interaction.options.getInteger('question');
+		if (questionNumber > wouldYouRather.length) return await interaction.reply({ content: 'No such question found', ephemeral: true });
+		let reply = `**__Here's our ${ordinal(questionNumber)} Would You Rather:__**
+\n**Would you rather**
+🔵 : ${wouldYouRather[questionNumber - 1][0]}
+🔴 : ${wouldYouRather[questionNumber - 1][1]}
+\nVote for your answers below!`;
+		const message = await interaction.reply({ content: reply, fetchReply: true });
+		message.react('🔵');
+		message.react('🔴');
 	},
 };
